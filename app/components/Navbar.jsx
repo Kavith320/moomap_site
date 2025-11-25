959dee6691fd899b4cb01c9522851e12d876f396
Kavith320 <kavith.b.320udapola@gmail.com>
2025-11-25 15:27:19 +0530

    Update site code

--- app/components/Navbar.jsx
+++ app/components/Navbar.jsx
@@ -5,7 +5,7 @@ export default function Navbar() {
         <nav className={styles.nav}>
             <div className={styles.container}>
                 {/* Logo + Name (same row) */}
-                <a href="/" className={styles.brand}>
+                <a href="" className={styles.brand}>
                 <img src="/logo.jpg" alt="MooMap Logo" className={styles.logo} />
 
                     <span className={styles.name}>MooMap</span>
@@ -18,7 +18,7 @@ export default function Navbar() {
                     <li><a href="#about" className={styles.link}>About</a></li>
                     <li><a href="#download" className={styles.link}>Download</a></li>
                     <li><a href="#gallery" className={styles.link}>Gallery</a></li>
-                    <li><a href="#team" className={styles.link}>Team</a></li>\
+                    <li><a href="#team" className={styles.link}>Team</a></li>
                     <li><a href="#contact" className={styles.link}>Contact</a></li>
                 </ul>
             </div>
